2fd2b0f55e4b32da29f2b78f29d39042263b6e24
arwakamal <arwakamal336@gmail.com>
2022-08-14 17:48:58 +0200

    Commit

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -1,14 +1,62 @@
 package com.example.android.trackmysleepquality.sleepquality
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.*
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 
-class SleepTrackerViewModel(
-        val database: SleepDatabaseDao,
-        application: Application) : AndroidViewModel(application)
+class SleepQualityViewModel(
+        /**
+         *Our ViewModel needs access to the data in the database,which is through the interface defined in the DAO.
+         *So we pass in an instance of the SleepDatabaseDao.
+         *
+         **/
+        private val sleepNightKey: Long = 0L,
+        val database: SleepDatabaseDao):ViewModel()
 {
+        private var viewModelJob= Job()
+        private val uiScope= CoroutineScope(Dispatchers.Main+viewModelJob)
 
+        /**
+         * Variable that tells the fragment whether it should navigate to [SleepTrackerFragment].
+         *
+         * This is `private` because we don't want to expose the ability to set [MutableLiveData] to
+         * the [Fragment]
+         */
+        private val _navigateToSleepTracker = MutableLiveData<Boolean?>()
+        /**
+         * When true immediately navigate back to the [SleepTrackerFragment]
+         */
+        val navigateToSleepTracker: LiveData<Boolean?>
+                get() = _navigateToSleepTracker
+        /**
+         * Call this immediately after navigating to [SleepTrackerFragment]
+         */
+        fun doneNavigating() {
+                _navigateToSleepTracker.value = null
+        }
+        /**
+         * Sets the sleep quality and updates the database.
+         *
+         * Then navigates back to the SleepTrackerFragment.
+         */
+        fun onSetSleepQuality(quality: Int) {
+                viewModelScope.launch withContext@{
+                        val tonight = database.get(sleepNightKey) ?: return@withContext
+                        tonight.sleepQuality = quality
+                        database.update(tonight)
+                     // Setting this state variable to true will alert the observer and trigger navigation.
+                        _navigateToSleepTracker.value = true
+                }
+        }
+        override fun onCleared() {
+                super.onCleared()
+                viewModelJob.cancel()
+        }
 
 }
--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -1,25 +1,13 @@
-/*
- * Copyright 2018, The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.android.trackmysleepquality.sleeptracker
 
 import android.app.Application
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.AndroidViewModel
+import android.provider.SyncStateContract.Helpers.insert
+import android.provider.SyncStateContract.Helpers.update
+import androidx.lifecycle.*
 import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import com.example.android.trackmysleepquality.database.SleepNight
+import com.example.android.trackmysleepquality.formatNights
+import kotlinx.coroutines.*
 
 /**
  * ViewModel for SleepTrackerFragment.
@@ -27,5 +15,111 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
 class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
         application: Application) : AndroidViewModel(application) {
+
+            private var viewModelJob= Job()
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+    /**
+     * the scope determines what thread the coroutine will run on , and it also need to about the job */
+    private val uiScope= CoroutineScope(Dispatchers.Main+viewModelJob)
+    /**tonight->to hold the current night.+we make this live data because we want to be able to observe it
+    MutableLiveData ->so that we can change it.**/
+    
+    private var tonight = MutableLiveData<SleepNight?>()
+    private val nights = database.getAllNights()
+    val nightsString = Transformations.map(nights) { nights ->
+        formatNights(nights, application.resources)
+    }
+
+    private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
+
+    val navigateToSleepQuality: LiveData<SleepNight>
+        get() = _navigateToSleepQuality
+    fun doneNavigating() {
+        _navigateToSleepQuality.value = null
+    }
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        /**we are using a coroutines to get tonight form the database.
+         * so that we are not blocking the ui while waiting for the result.**/
+        //in this scope we creating coroutine
+        //creates coroutine without blocking the current thread in the context defined by the scope.
+       uiScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase():  SleepNight?
+    {
+        return withContext(Dispatchers.IO)
+        {
+            var night = database.getTonight()
+
+            if (night?.endTimeMilli != night?.startTimeMilli) {
+                night = null
+            }
+            night
+        }
+
+
+    }
+    fun onStartTracking()
+    {
+        uiScope.launch{
+            val newNight = SleepNight()
+            insert(newNight)
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun insert(night: SleepNight) {
+        withContext(Dispatchers.IO){
+            database.insert(night)
+        }
+    }
+    
+//    fun someWorkNeedsToBeDone(){
+//        uiScope.launch {
+//            suspendFunction()
+//        }
+//    }
+//
+//    private suspend fun suspendFunction() {
+//        withContext(Dispatchers.IO){
+//            longruningwork()
+//        }
+//    }
+    fun onStopTracking() {
+        viewModelScope.launch {
+            val oldNight = tonight.value ?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+
+            _navigateToSleepQuality.value = oldNight
+        }
+
+
+}
+
+    private suspend fun update(night: SleepNight) {
+        database.update(night)
+    }
+    fun onClear() {
+        viewModelScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    suspend fun clear() {
+        database.clear()
+    }
 }
 
